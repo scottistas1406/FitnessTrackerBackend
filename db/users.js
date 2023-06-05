877e7d9e27d6138944f59f3daa88a99c00ab6c60
scottistas1406 <scottistas@gmail.com>
2023-06-05 18:45:38 -0500

    added bcrypt

--- db/users.js
+++ db/users.js
@@ -1,13 +1,19 @@
 const client = require("./client");
+const bcrypt=require('bcrypt');
+const SALT_COUNT = 10;
 
 // database functions
 
 // user functions
 async function createUser({ username, password }) {
+  
+  const hashedPassword = await bcrypt.hash(password, SALT_COUNT)
   try {
+    
     const query = 'INSERT INTO users (username, password) VALUES ($1, $2) RETURNING *';
     const values = [username, password];
     const result = await client.query(query, values);
+    
     return result.rows[0];
   } catch (error) {
     console.error('Error creating user');
@@ -16,7 +22,21 @@ async function createUser({ username, password }) {
 }
 
 async function getUser({ username, password }) {
-  // Implement the logic to retrieve a user by their username and password
+  if (!username || !password) {
+    return;
+  }
+
+  try {
+    const user = await getUserByUsername(username);
+    if (!user) return;
+    const hashedPassword = user.password;
+    const passwordsMatch = await bcrypt.compare(password, hashedPassword);
+    if (!passwordsMatch) return;
+    delete user.password;
+    return user;
+  } catch (error) {
+    throw error;
+  }
 }
 
 async function getUserById(userId) {
