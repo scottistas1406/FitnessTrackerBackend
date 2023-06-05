f6ddd57760ddc96458bfd73b25a46830a6ab37c2
scottistas1406 <scottistas@gmail.com>
2023-06-05 17:25:31 -0500

    created users table and started seed requirements

--- db/seedData.js
+++ db/seedData.js
@@ -1,5 +1,7 @@
 // require in the database adapter functions as you write them (createUser, createActivity...)
 // const { } = require('./');
+const {createUser}= require('./');
+
 const client = require("./client")
 
 async function dropTables() {
@@ -8,9 +10,22 @@ async function dropTables() {
 }
 
 async function createTables() {
-  console.log("Starting to build tables...")
-  // create all tables, in the correct order
+  console.log("Starting to build tables...");
+  try {
+    await client.query(`
+      CREATE TABLE users (
+        id SERIAL PRIMARY KEY,
+        username VARCHAR(255) UNIQUE NOT NULL,
+        password VARCHAR(255) NOT NULL
+      );
+    `);
+  } catch (error) {
+    console.error('Error building user');
+    throw error;
+  }
 }
+  // create all tables, in the correct order
+
 
 /* 
 
--- db/users.js
+++ db/users.js
@@ -4,19 +4,27 @@ const client = require("./client");
 
 // user functions
 async function createUser({ username, password }) {
-  
+  try {
+    const query = 'INSERT INTO users (username, password) VALUES ($1, $2) RETURNING *';
+    const values = [username, password];
+    const result = await client.query(query, values);
+    return result.rows[0];
+  } catch (error) {
+    console.error('Error creating user');
+    throw error;
+  }
 }
 
 async function getUser({ username, password }) {
-
+  // Implement the logic to retrieve a user by their username and password
 }
 
 async function getUserById(userId) {
-
+  // Implement the logic to retrieve a user by their user ID
 }
 
 async function getUserByUsername(userName) {
-
+  // Implement the logic to retrieve a user by their username
 }
 
 module.exports = {
@@ -24,4 +32,5 @@ module.exports = {
   getUser,
   getUserById,
   getUserByUsername,
-}
+};
+
