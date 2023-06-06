a0a9acd8789991be976903f8dddc24d8362313be
scottistas1406 <scottistas@gmail.com>
2023-06-06 11:46:13 -0500

    fixed syntax on object

--- db/routines.js
+++ db/routines.js
@@ -85,7 +85,6 @@ async function updateRoutine({ id, ...fields }) {
   const setString = Object.keys(fields)
     .map((key, index) => `"${key}"=$${index + 1}`)
     .join(", ");
-  // return early if this is called without fields
   if (setString.length === 0) {
     return;
   }
