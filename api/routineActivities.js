e9e89fe0ae6b2fcd0d36613a7f36343996de0ef3
AdmiralLag <admirallag@gmail.com>
2023-07-09 13:25:53 -0500

    Indiana Jones and The Last Commit

--- api/routineActivities.js
+++ api/routineActivities.js
@@ -1,8 +1,68 @@
 const express = require('express');
+const client = require("../db/client");
+//const { requireUser } = require("./utils");
 const router = express.Router();
+const { updateRoutineActivity, getRoutineActivityById, getRoutineById, destroyRoutineActivity} = require("../db");
+const{ UnauthorizedUpdateError, UnauthorizedDeleteError } = require('../errors')
+
+
+function requireUser(req, res, next) {
+    console.log("req.user:", req.user);
+    if (!req.user) {
+        next({
+            name: "MissingUserError",
+            message: "You must be logged in to perform this action"
+        });
+    }
+
+    next();
+}
+
 
 // PATCH /api/routine_activities/:routineActivityId
-
+router.patch('/:routineActivityId', requireUser, async(req, res, next) => {
+    const {routineActivityId} = req.params;
+    const {count, duration} = req.body;
+    const originalRoutineActivity = await getRoutineActivityById(routineActivityId)
+    const {routineId} = originalRoutineActivity
+    const routine = await getRoutineById(routineId)
+    try {
+        if(originalRoutineActivity && routine.creatorId === req.user.id){
+            const updatedRoutineActivity = await updateRoutineActivity({id: routineActivityId, count, duration});
+            res.send(updatedRoutineActivity)
+        } else {
+            res.status(403);
+            res.send({
+                error: 'UnauthorizedUpdateError',  
+                message: `User ${req.user.username} is not allowed to update ${routine.name}`,
+                name: UnauthorizedUpdateError()
+            })
+        }
+    } catch ({name, message}) {
+        next({name, message})
+    }
+});
 // DELETE /api/routine_activities/:routineActivityId
+router.delete('/:routineActivityId', requireUser, async(req, res, next) => {
+    const {routineActivityId} = req.params;
+    const routineActivityToDelete = await getRoutineActivityById(routineActivityId);
+    const {routineId} = routineActivityToDelete;
+    const routine = await getRoutineById(routineId);
+    try {
+        if(routineActivityToDelete && routine.creatorId === req.user.id){
+            const destroyedRoutineActivity = await destroyRoutineActivity(routineActivityId);
+            res.send(destroyedRoutineActivity)
+        } else{
+            res.status(403);
+            res.send({
+                error: 'UnauthorizedDeleteError',
+                message: `User ${req.user.username} is not allowed to delete ${routine.name}`,
+                name: UnauthorizedDeleteError()
+            })
+        }
+    } catch ({name, message}) {
+        next({name, message})
+    }
+});
 
 module.exports = router;
--- api/routines.js
+++ api/routines.js
@@ -1,54 +1,152 @@
 const express = require('express');
 const router = express.Router();
 
-const {getRoutineById, addActivityToRoutine, getAllPublicRoutines, createRoutine, updateRoutine, destroyRoutine, getRoutineActivitiesByRoutine } = require('../db');
+const { getRoutineById, addActivityToRoutine, getAllPublicRoutines, createRoutine, updateRoutine, destroyRoutine, getRoutineActivitiesByRoutine } = require('../db');
 
-const {UnauthorizedError, UnauthorizedUpdateError, UnauthorizedDeleteError} = require('../errors');
-// GET /api/routines
+const { UnauthorizedError, UnauthorizedUpdateError, UnauthorizedDeleteError, DuplicateRoutineActivityError } = require('../errors');
 
-router.get('/', async(req, res, next) => {
-    try {
-        const routines = await getAllPublicRoutines();
+router.get('/', async (req, res, next) => {
+  try {
+    const routines = await getAllPublicRoutines();
 
-        if(routines){
-            res.send(routines)
-        }
-    } catch ({name, message}) {
-        next({name, message})
+    if (routines) {
+      res.send(routines);
     }
+  } catch ({ name, message }) {
+    next({ name, message });
+  }
 });
 
-
-
-// POST /api/routines
 router.post('/', async (req, res, next) => {
-    const { isPublic, name, goal } = req.body;
-    user = req.body;
-    //console.log(user)
-    if (!user) {
-      res.send({
-        error: 'No user found',
-        message: UnauthorizedError(),
-        name: 'Unauthorized Error'
-      })
-    }
-    try {
-      const newRoutine = await createRoutine({
-        creatorId: req.user.id, 
-        isPublic: isPublic,
-        name: name,
-        goal: goal
+  const { isPublic, name, goal } = req.body;
+
+  if (!req.user) {
+    res.status(401).send({
+      error: 'Unauthorized',
+      message: 'No user found',
+      name: UnauthorizedError()
+    });
+    return;
+  }
+
+  try {
+    const newRoutine = await createRoutine({
+      creatorId: req.user.id,
+      isPublic: isPublic,
+      name: name,
+      goal: goal
+    });
+
+    res.send(newRoutine);
+  } catch ({ name, message }) {
+    next({ name, message });
+  }
+});
+
+router.patch('/:routineId', async (req, res, next) => {
+  const { routineId } = req.params;
+  const { isPublic, name, goal } = req.body;
+  const updateFields = { id: routineId };
+
+  if (!req.user) {
+    res.status(401).send({
+      error: 'Unauthorized',
+      message: 'No user found',
+      name: UnauthorizedError()
+    });
+    return;
+  }
+
+  if (name) {
+    updateFields.name = name;
+  }
+  if (goal) {
+    updateFields.goal = goal;
+  }
+  if (isPublic !== undefined) {
+    updateFields.isPublic = isPublic;
+  }
+
+  try {
+    const routine = await getRoutineById(routineId);
+
+    if (req.user.id !== routine.creatorId) {
+      res.status(403).send({
+        error: 'Unauthorized',
+        message: 'Unauthorized to update this routine',
+        name: UnauthorizedUpdateError()
       });
-      res.send(newRoutine);
-    }catch ({name, message}) {
-      next({name, message})
+      return;
     }
-  });
 
-// PATCH /api/routines/:routineId
+    const updatedRoutine = await updateRoutine(updateFields);
+    res.send(updatedRoutine);
+  } catch ({ name, message }) {
+    next({ name, message });
+  }
+});
 
-// DELETE /api/routines/:routineId
+router.delete('/:routineId', async (req, res, next) => {
+  const { routineId } = req.params;
 
-// POST /api/routines/:routineId/activities
+  if (!req.user) {
+    res.status(401).send({
+      error: 'Unauthorized',
+      message: 'No user found',
+      name: UnauthorizedError()
+    });
+    return;
+  }
+
+  try {
+    const routine = await getRoutineById(routineId);
+
+    if (req.user.id !== routine.creatorId) {
+      res.status(403).send({
+        error: 'Unauthorized',
+        message: 'Unauthorized to delete this routine',
+        name: UnauthorizedDeleteError()
+      });
+      return;
+    }
+
+    await destroyRoutine(routineId);
+    res.send(routine);
+  } catch ({ name, message }) {
+    next({ name, message });
+  }
+});
+
+router.post('/:routineId/activities', async (req, res, next) => {
+  const { routineId } = req.params;
+  const { activityId, count, duration } = req.body;
+
+  if (!req.user) {
+    res.status(401).send({
+      error: 'Unauthorized',
+      message: 'No user found',
+      name: UnauthorizedError()
+    });
+    return;
+  }
+
+  try {
+    const routine = await getRoutineById(routineId);
+
+    if (routine.creatorId !== req.user.id) {
+      res.status(403).send({
+        error: 'Unauthorized',
+        message: 'Error posting routine activities',
+        name: DuplicateRoutineActivityError()
+      });
+      return;
+    }
+
+    const updatedActivity = await addActivityToRoutine({ routineId, activityId, count, duration });
+    res.send(updatedActivity);
+  } catch ({ name, message }) {
+    next({ name, message });
+  }
+});
 
 module.exports = router;
